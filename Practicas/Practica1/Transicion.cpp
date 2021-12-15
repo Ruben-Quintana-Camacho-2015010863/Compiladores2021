e83e17792ad2000156ca3ede270dfb2bedcd3571
rubenqc <rubenxamacho1@gmail.com>
2021-12-15 01:10:21 -0600

    Definimos el metodo de evaluacion del estado

--- Practicas/Practica1/Transicion.cpp
+++ Practicas/Practica1/Transicion.cpp
@@ -2,9 +2,32 @@
 #include "Transicion.h"
 using namespace std;
 
-//Constructor
+//Constructor, segun yo cuenta como seter tambien :L
 Transicion::Transicion(int inicio, int fin, char simbolo){
     inicio = 0;
     fin = 0;
     simbolo = 'E';//Transicion epsilon
+}
+
+int Transicion::getInicio(){
+    return inicio;
+}
+
+int Transicion::getFin(){
+    return fin;
+}
+
+char Transicion::getSimbolo(){
+    return simbolo;
+}
+
+bool Transicion::equals(Transicion tr){
+    int in = tr.getInicio();
+    int fn = tr.getFin();
+    char c = tr.getSimbolo();
+    if( (inicio == in) && (fin == fn) && (simbolo == c) ){
+        return true;
+    }else{
+        return false;
+    }
 }
