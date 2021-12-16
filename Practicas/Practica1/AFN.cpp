9de8e56483d265208905450e4956c789983d4bd4
rubenqc <rubenxamacho1@gmail.com>
2021-12-16 17:37:12 -0600

    Empezamos a definir las transiciones del automata :p

--- Practicas/Practica1/AFN.cpp
+++ Practicas/Practica1/AFN.cpp
@@ -10,23 +10,31 @@ AFN::AFN(){
 
 }
 
-void carga(string nombre){
+void AFN::carga(string nombre){
     ofstream file; //Puede que sirva :L
     file.open(nombre, ios::out);  //Cargamos l archivo, si no existe se crea un archivo
 }
 
-void cargaTransiciones(){
+void AFN::cargaTransiciones(){
     try{
         string linea;   //Almacenamos la cadena que se escribe en el archivo
         int inicio, fin;
-        char c;
+        const char *c;
         std::regex expresion("(\\d+)->(\\d+),([a-zE])");    //Declaramos expresion
-        std::smatch expresionMatch;
+        std::cmatch expresionMatch;
         while(!(linea.empty())){
-            std::regex_search(linea, expresionMatch, expresion); //Creo que puede ir mejor en un if :L
-            //if(std::regex_search(linea, expresionMatch, expresion))
+            if(std::regex_search(linea, expresionMatch, expresion)){ //Creo que puede ir mejor en un if :L
+                inicio = stoi(expresionMatch[1]);
+                fin = stoi(expresionMatch[2]);
+                c = expresionMatch[3].first;
+                addTransicion(inicio, fin, c);  //Agremos una transicion al archivo
+            }
         }
     }catch(exception e){
 
     }
+}
+
+void AFN::addTransicion(int inicio, int fin, const char *simbolo){
+    //Transicion transicion = new Transicion(inicio, fin, simbolo);   //new??
 }
