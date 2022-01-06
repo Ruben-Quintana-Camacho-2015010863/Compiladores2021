abf1c68f5306a5132f1749590b64a0700b0ec353
rubenqc <rubenxamacho1@gmail.com>
2022-01-06 14:44:31 -0600

    Ahora necesito definir el AFD para poder continuar con el algoritmo, pero con la misma logica que la practica 1 :L

--- Practicas/Practica3/Subconjuntos.py
+++ Practicas/Practica3/Subconjuntos.py
@@ -1,8 +1,71 @@
-import AFN
+from re import U
+import AFN,Transiciones
+
+class Subconjuntos(object):
+    def __init__(self, afn):
+        self.afn = afn
+        self.Destados = {}
+
+    def e_cerradura(self, estados):     #cerradura epsilon
+        Tr = self.afn.trancisiones
+        cerradura = []
+        cerradura.append(estados)   #almacenamos los estados del automara para su analisis
+        pila = []
+        pila.append(estados)    #una estructura auxiliar
+        while len(pila) != 0:
+            estado = pila.pop()
+            #---------
+            for transicion in Tr:
+                if transicion.inicio == estado and transicion.simbolo == 'E':
+                    if transicion.fin not in cerradura:
+                        cerradura.append(transicion.fin)
+                        pila.append(transicion.fin)
+        return cerradura
+
+    def mover(self, estados, simbolo):   #Trnaisicion
+        Tr = self.afn.transiciones
+        m = []
+        for estado in estados:
+            for transicion in Tr:
+                if estado == transicion.inicio and transicion.simbolo == simbolo:
+                    m.append(transicion.fin)
+        return m
+    
+    def construirSubconjunto(self):
+        etiqueta = 65
+        seEncuentra = False     #No se me ocurre otro nombre de variable XD
+        self.Destados[str(chr(etiqueta))] = (self.e_cerradura(self.afn.inicial))
+        self.Destados[str(chr(etiqueta))].sort()
+        self.Destados[str(chr(etiqueta))].append(False)
+        while not self.analizarMarcados():
+            self.Destados[str(chr(etiqueta))][-1] = True    #-1 es de der a izq
+            for simbolo in self.afn.alfabeto:
+                U = self.e_cerradura(self.mover(self.Destados[str(chr(etiqueta))][:-1], simbolo))
+                U.sort()    #u es una var
+                for key, val in self.Destados.items():
+                    if all(item in U for item in val):
+                        seEncuentra = True
+                    if not seEncuentra:
+                        etiqueta = etiqueta + 1
+                        self.Destados[str(chr(etiqueta))] = U
+                        self.Destados[str(chr(etiqueta))].append(True)
+                        #necesito declarar el afd
+
+
+    
+    def analizarMarcados(self):
+        marcados = True
+        for key, val in self.Destados.items():
+            if val[-1] == False:
+                marcados = False
+        return marcados
+
+
+        
 
 if __name__ == "__main__":
     autn = AFN.AFN()
-    autn.carga("afn.afn")
+    autn.carga("..\\afn.afn")
     autn.cargaTransiciones()
     autn.obtenerInicial()
     autn.obtenerFinal()
